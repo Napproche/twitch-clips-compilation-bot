252758e97948827cb5b19b83a607fe47c04ec1a5
michielst <michieldegreef@outlook.com>
2018-03-15 23:05:39 +0100

    YouTube upload flow

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # twitch-clip-publisher
 Bot that creates and uploads compilations of popular Twitch clips each day/week/month
+
+## YouTube Client
+https://developers.google.com/api-client-library/python/samples/samples
--- script.py
+++ script.py
@@ -5,27 +5,27 @@ from services import youtube as youtubeService
 BLACKLISTED_CHANNELS = ["DisguisedToastHS"]
 
 if __name__ == "__main__":
-    clips = []
+    # clips = []
     
-    response = twitchService.getTwitchClips(period='day', game='Fortnite', limit=1)
+    # response = twitchService.getTwitchClips(period='week', game='Fortnite', limit=10)
     
-    for clip in response['clips']:
-        # Check if channel isn't blacklisted
-        if clip['broadcaster']['display_name'] not in BLACKLISTED_CHANNELS:
-            # Save clip data. TODO: Save to database
-            clips.append({
-                'title': clip['title'],
-                'channel': clip['broadcaster']['display_name'],
-                'slug': clip['slug'],
-                'game': clip['game'],
-                'date': clip['created_at'],
-                'views': clip['views'],
-                'duration': clip['duration'],
-            })
+    # for clip in response['clips']:
+    #     # Check if channel isn't blacklisted
+    #     if clip['broadcaster']['display_name'] not in BLACKLISTED_CHANNELS:
+    #         # Save clip data. TODO: Save to database
+    #         clips.append({
+    #             'title': clip['title'],
+    #             'channel': clip['broadcaster']['display_name'],
+    #             'slug': clip['slug'],
+    #             'game': clip['game'],
+    #             'date': clip['created_at'],
+    #             'views': clip['views'],
+    #             'duration': clip['duration'],
+    #         })
 
-            # Download clip
-            twitchService.downloadTwitchClip('downloads/', clip)
+    #         # Download clip
+    #         twitchService.downloadTwitchClip('downloads/', clip)
 
-    moviePyService.createVideoOfListOfClips(clips)
+    # moviePyService.createVideoOfListOfClips(clips)
 
     youtubeService.uploadVideoToYouTube()
