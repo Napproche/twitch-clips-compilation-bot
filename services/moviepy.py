4a3f3035491bede9184f17699469d91ee95ab3c8
michielst <michieldegreef@outlook.com>
2018-03-18 20:17:54 +0100

    Clip downloads location constant

--- services/moviepy.py
+++ services/moviepy.py
@@ -1,10 +1,12 @@
 from moviepy.editor import VideoFileClip, concatenate_videoclips, CompositeVideoClip, TextClip
 
+import constants
+
 def createVideoOfListOfClips(clips, output):
     final_clips = []
 
     for clip in clips:
-        video = VideoFileClip("downloads/" + clip['channel'] + '/' + clip['slug'] + '.mp4')
+        video = VideoFileClip(constants.DOWNLOAD_LOCATION + clip['channel'] + '/' + clip['slug'] + '.mp4')
         title = TextClip(clip['channel'] + ': ' + clip['title'], font='Amiri-regular', color='white', fontsize=55).set_duration(8)
         title_mov = title.set_pos((0.05,0.8), relative=True)
 
